e9c8d8002f1d449ae57a156228fa08324236ed04
sm64isdabest <thiagopinheiro301007@gmail.com>
2026-03-27 10:50:31 -0300

    feat: return func for close button in about section

--- js/switch-screen.js
+++ js/switch-screen.js
@@ -9,6 +9,9 @@ const play_area = document.getElementById("play_area");
 const play_button = document.getElementById("play_button");
 const about_button = document.getElementById("about_button");
 
+// (close button found inside the about section)
+const about_close = document.getElementById("close_button");
+
 play_button.addEventListener("click", () => {
     menu.style = "display: none;";
     play_area.style = "display: flex;";
@@ -19,4 +22,9 @@ play_button.addEventListener("click", () => {
 about_button.addEventListener("click", () => {
     menu.style = "display: none;";
     about.style = "display: flex;";
+});
+
+about_close.addEventListener("click", () => {
+    menu.style = "display: flex;";
+    about.style = "display: none;";
 });
